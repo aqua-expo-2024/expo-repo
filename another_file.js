8e3696a82430a7599524656be183e0ec1c980062
MorAlon1 <mor.alon@aquasec.com>
2024-02-08 14:42:26 +0200

    remove req url

---
+++ another_file.js
@@ -0,0 +1,12 @@
+import _ from "lodash";
+
+console.log(_.zipObjectDeep(["a.b[0].c", "a.b[1].d"], [1, 2]));
+console.log(_.toNumber("3.2"));
+console.log(_.template());
+
+
+
+
+
+
+console.log(_.template());
